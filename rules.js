23a423a5249e46c6c84f22c19349a0e52cf9705f
Akhil Daphara <akhildaphara@gmail.com>
2020-08-05 16:12:08 +0530

    Added test

--- rules.js
+++ rules.js
@@ -1,7 +1,7 @@
 var _ = require("lodash");
 
 const rule1 = function (cd, td, oc) {
-  console.log("Inside rule1");
+  //console.log("Inside rule1");
   if (oc.termsFilter.channel.includes(td.channel)) return rule2(cd, td, oc);
   else
     return {
@@ -15,7 +15,7 @@ const rule1 = function (cd, td, oc) {
 };
 
 const rule2 = function (cd, td, oc) {
-  console.log("Inside rule2");
+  //console.log("Inside rule2");
   if (oc.termsFilter.transTypeCode.includes(td.transTypeCode)) return true;
   else
     return {
@@ -29,7 +29,7 @@ const rule2 = function (cd, td, oc) {
 };
 
 const rule3 = function (cd, td, oc) {
-  console.log("Inside rule3");
+  //console.log("Inside rule3");
   if (
     _.intersection(oc.termsFilter.customerCategory, td.customerCategory)
       .length > 0
@@ -47,7 +47,7 @@ const rule3 = function (cd, td, oc) {
 };
 
 const rule4 = function (cd, td, oc) {
-  console.log("Inside rule4");
+  //console.log("Inside rule4");
   let cur = [];
   _.forEach(oc3.termsFilter.currency, (currency) => {
     cur.push(currency.currCode);
@@ -160,7 +160,23 @@ const rule8 = function (cd, td, oc) {
     };
 };
 
-//console.log(rule1(cd3, td3, oc3));
+let cd = {
+  cif: "12345",
+  customerCategory: ["EMPLOYEE", "WEALTH"],
+  usedCodes: [
+    {
+      codeType: "P",
+      codeName: "KOTAKFIRST",
+      usedCount: "1",
+    },
+    {
+      codeType: "D",
+      codeName: "STUDENT",
+      usedCount: "2",
+    },
+  ],
+};
+
 module.exports = {
   rule1,
   rule2,
@@ -171,3 +187,73 @@ module.exports = {
   rule7,
   rule8,
 };
+
+/*
+let td = {
+  requestID: 1,
+  transDate: "01-04-2020",
+  transTypeCode: "SVC-R",
+  currency: "USD",
+  amount: 1500,
+  promoCode: "KOTAKFIRST",
+  channel: "PORTAL",
+  rate: 77.31,
+  ibr: 75.31,
+  cardRate: 77.31,
+  perUnit: 1,
+  buySellSign: -1,
+  orgCharges: 100,
+  lcyAmount: 115965,
+};
+let oc = {
+  codeType: "D",
+  validFor: "RC",
+  codeName: "STUDENT",
+  description:
+    " DISCOUNT FOR EDUCATIONAL SEASON- CHARGES Rs 100 OFF, 50 PAISE DISCOUNT ON CARD RATE",
+  startDateTime: "01-04-2020 00:00:00",
+  endDateTime: "01-07-2020 00:00:00",
+  minMaxAmountType: "LCY",
+  minimumINRAmount: "1000",
+  maximumINRAmount: "1000000",
+  maximumTotalUsage: "100000",
+  maximumUsagePerCustomer: "1",
+  rateApplyType: "GBL",
+  applicableRateMargin: "50",
+  chargesDiscount: {
+    chargeDiscountType: "F",
+    chargeDiscount: "100",
+  },
+  rateDiscount: {
+    rateDiscountType: "F",
+    rateDiscountOn: "IBR",
+    rateDiscountOrMargin: "50",
+  },
+  termsFilter: {
+    channel: ["branch", "mobile"],
+    transTypeCode: ["CN-SALE", "SVC-S", "SVC-R", "TT-SALE", "DD-SALE"],
+    customerCategory: ["STUDENT", "EMPLOYEE"],
+    currency: [
+      {
+        currCode: "USD",
+        minAmount: "",
+        maxAmount: "",
+        discount: "",
+      },
+      {
+        currCode: "EUR",
+        minAmount: "",
+        maxAmount: "",
+        discount: "",
+      },
+      {
+        currCode: "GBP",
+        minAmount: "",
+        maxAmount: "",
+        discount: "",
+      },
+    ],
+  },
+};
+console.log(rule1(cd, td, oc));
+*/
